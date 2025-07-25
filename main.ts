3c14997ae580e50c22d34a2080e40f9571ed41ff
Ashish Gurjar <ashishkgurjar@gmail.com>
2025-07-25 09:01:28 +0530

    upgrade to v45

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
-import { App, fsRoutes, staticFiles } from "fresh";
+import { App, staticFiles } from "fresh";
 import { define, type State } from "./utils.ts";
 
 export const app = new App<State>();
 
-app.use(staticFiles());
+app.use(staticFiles()).fsRoutes();
 
 // this is the same as the /api/:name route defined via a file. feel free to delete this!
 app.get("/api2/:name", (ctx) => {
@@ -20,11 +20,6 @@ const exampleLoggerMiddleware = define.middleware((ctx) => {
 });
 
 app.use(exampleLoggerMiddleware);
-
-await fsRoutes(app, {
-  loadIsland: (path) => import(`./islands/${path}`),
-  loadRoute: (path) => import(`./routes/${path}`),
-});
 
 if (import.meta.main) {
   await app.listen();
